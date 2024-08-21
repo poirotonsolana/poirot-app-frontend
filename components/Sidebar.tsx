cfb2dae65d09a47028f792a474240bde1e7a9b39
Dominik Gabela <gabela.dominik@gmail.com>
2024-08-21 18:12:41 +0200

    Refactor Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -21,31 +21,60 @@ import {
    
   export default function Sidebar() {
     return (
-        <Card className="h-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5 border-r-2">
+        <Card
+          className="h-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5 border-r-2"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
           <div className="mb-2 p-4 flex items-center justify-center">
-            <Typography variant="h5" color="blue-gray">
-              <Link href={"/"}>
-                  <Image
-                      src={poirotLogo}
-                      alt="logo"
-                      width={58}
-                      height={48}
+          <Typography 
+            variant="h5" 
+            color="blue-gray" 
+            placeholder="Enter text" 
+            onPointerEnterCapture={() => {}} 
+            onPointerLeaveCapture={() => {}}
+          >              
+          <Link href={"/"}>
+              <Image
+                 src={poirotLogo}
+                  alt="logo"
+                  width={58}
+                  height={48}
                   />
               </Link>
             </Typography>
           </div>
-          <List>
+          <List
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
             <Link href={"/contests"}>
-                <ListItem className="text-lg">
-                  <ListItemPrefix>
+                <ListItem
+                  className="text-lg"
+                  placeholder={undefined}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                >
+                  <ListItemPrefix
+                    placeholder={undefined}
+                    onPointerEnterCapture={undefined}
+                    onPointerLeaveCapture={undefined}
+                  >
                     <PresentationChartBarIcon className="h-5 w-5" />
                   </ListItemPrefix>
                   Contests
                 </ListItem>
             </Link><hr className="h-3 my-2 w-48 border-t-2"/>
             <Link href={"/leaderboard"}>
-                <ListItem className="text-lg">
-                  <ListItemPrefix>
+                <ListItem
+                  className="text-lg"
+                  placeholder={undefined}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                >
+                  <ListItemPrefix placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                     <LeaderboardIcon/>
                   </ListItemPrefix>
                   Leaderboard
